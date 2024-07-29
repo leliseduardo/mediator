c1274eb84fc6f766418b43b8b557ad288804995b
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2024-07-29 17:05:14 -0300

    init

---
+++ src/test/java/org/example/mediator/MediatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mediator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
